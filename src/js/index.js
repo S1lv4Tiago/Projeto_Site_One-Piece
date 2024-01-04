f0995502eab429d688eec7e135a7b78bce8e3ce4
Tiago Silva <silvatwebanlsds@outlook.com>
2024-01-03 22:53:23 -0300

    Add files via upload
    In 3 lives (HTML, CSS and JAVASCRIPT), I learned how the structure of HTML works, stylizations with CSS and the interactivity of JavaScript.

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+//Objetivo 1 - quando clicar no botão do personagem na lista, marcar o botão como selecionado.
+
+//passo 1 objtv 1 - pegar os botões no JS pra poder verificar quando o usuário clicar em cima de um deles
+const botoes = document.querySelectorAll('.botao');
+
+//Objetivo 2 - quando clicar no botão do personagem mostrar as informações do personagem
+//passo - 1 pegar os personagens no JS para mostrar ou esconder
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () =>{
+        
+        const botaoSelecionado = document.querySelector(".botao.selecionado");
+        botaoSelecionado.classList.remove("selecionado");
+
+        botao.classList.add("selecionado");
+
+
+        //passo 3 objtv 1 - verificar se já existe um botão selecionado, se sim, devemos remover a seleção dele
+         const personagemselecionado = document.querySelector(".personagem.selecionado");
+         personagemselecionado.classList.remove("selecionado");
+
+
+
+        //passo 2 objtv 1 - adicionar a classe "selecionado" no botão que o usuário clicou.
+        personagens[indice].classList.add("selecionado");
+        console.log(indice);
+    });
+});
+
+
+
+    
+
+
+
+
+
